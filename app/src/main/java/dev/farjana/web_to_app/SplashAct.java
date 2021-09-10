fb856f7b5c8d7da04b144274d7d90a6dea5f4929
Farjana <farjanaakter431@gmail.com>
2021-09-10 21:20:28 +0600

    splash added

---
+++ app/src/main/java/dev/farjana/web_to_app/SplashAct.java
@@ -0,0 +1,38 @@
+package dev.farjana.web_to_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
+public class SplashAct extends AppCompatActivity {
+
+    CircleImageView splashImageView;
+    Handler handler;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        splashImageView = findViewById(R.id.splashImage);
+
+
+        Intent receiverIntent = getIntent();
+        String searchUrl = receiverIntent.getStringExtra("Url");
+        int imageId = receiverIntent.getIntExtra("ImageId",1);
+
+        splashImageView.setImageResource(imageId);
+
+        handler = new Handler();
+        handler.postDelayed((Runnable) () -> {
+            Intent intent = new Intent(SplashAct.this,WebViewScreen.class);
+            intent.putExtra("Url",searchUrl);
+            startActivity(intent);
+            finish();
+        },5000);
+
+
+    }
+}
